abc2ec39d0c9bda4439350ab481282833b772812
Eino Korte <e.i.korte@gmail.com>
2021-11-28 22:50:49 +0200

    added docker things and interp

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +6,7 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
 	}
 };
 
